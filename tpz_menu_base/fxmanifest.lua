30580860979f02afdcf76b8323c47763529164ee
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2024-07-17 19:55:26 +0300

    Add files via upload

--- tpz_menu_base/fxmanifest.lua
+++ tpz_menu_base/fxmanifest.lua
@@ -1,10 +1,7 @@
 fx_version 'adamant'
-
 game 'rdr3'
-
+author 'Nosmakos' --  SLIZZARN original author
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
-
-version '1.0.0'
 
 client_scripts {
 	'client/main.lua'
@@ -20,9 +17,5 @@ files {
 	'html/css/*.png',
 	'html/js/mustache.min.js',
 	'html/js/app.js',
-	'html/js/wrapper.js',
-	'html/fonts/crock.ttf',
-	'html/fonts/HapnaSlabSerif-Medium.ttf',
-	'html/fonts/bankgothic.ttf'
+	'html/fonts/*ttf',
 }
-
